0362eb689ed164836d3ced6afb5097afdda0338f
Ilya Abramovich <ilyaabramovich1998@gmail.com>
2026-05-23 01:06:38 +0300

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        res = 1
+        indices = sorted(range(len(position)), key = lambda x: -position[x])
+        times = [(target - position[i]) / speed[i] for i in indices]
+        stack = [times[0]]
+        for i in range(1, len(times)):
+            if stack[-1] < times[i]:
+                res += 1
+                stack.append(times[i])
+        return len(stack)
